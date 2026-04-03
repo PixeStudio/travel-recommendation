a55f739041a7b5faf0aa64bfeb1a4e99d8717954
PixeStudio <56841910+PixeStudio@users.noreply.github.com>
2026-04-03 23:42:18 +0200

    feat: implement search query normalization (Task 7)

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -6,3 +6,21 @@ fetch("travel_recommendation_api.json")
   .catch(error => {
     console.error("Error fetching data:", error);
   });
+
+  const input = document.querySelector(".search-panel input");
+  const searchBtn = document.querySelector(".b-search");
+
+  function noramlizeQuery(query) {
+    if (query.includes("beach")) return "beach";
+    if (query.includes("temple")) return "temple";
+    if (query.includes("country")) return "country";
+    return null;        
+  }
+
+  searchBtn.addEventListener("click", () => {
+    const rawQuery = input.value.toLowerCase(); 
+    const query = noramlizeQuery(rawQuery);
+    console.log("Normalized: ", query);
+  });
+
+  
